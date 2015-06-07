68ab357e7ca671b6960f2bcf12a4821c2a2cf28e
Gregg Mojica <gwmojica@gmail.com>
2015-06-07 01:19:04 -0500

    first commit

---
+++ ErrorBar.h
@@ -0,0 +1,13 @@
+//
+//  ErrorBar.h
+//  DepthAlert
+//
+//  Created by Gregg Mojica on 6/6/15.
+//  Copyright (c) 2015 Gregg Mojica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ErrorBar : UIView
+
+@end
---
+++ ErrorBar.m
@@ -0,0 +1,21 @@
+//
+//  ErrorBar.m
+//  DepthAlert
+//
+//  Created by Gregg Mojica on 6/6/15.
+//  Copyright (c) 2015 Gregg Mojica. All rights reserved.
+//
+
+#import "ErrorBar.h"
+
+@implementation ErrorBar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
